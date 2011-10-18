506b309187dffbfbfd15bbc2238c78d5ad532f58
kviny <kviny@ubuntu.ubuntu-domain>
2011-10-19 01:37:35 +0400

    Implemented creating ticket for a project.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Ticketee::Application.routes.draw do
+  root :to => 'projects#index'
   
-  resources :projects
+  resources :projects do
+    resources :tickets
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +53,7 @@ Ticketee::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'projects#index'
+
 
   # See how all your routes lay out with "rake routes"
 
